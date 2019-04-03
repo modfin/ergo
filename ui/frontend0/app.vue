bae0b6f5335c1dc95b224ad837310276500b386e
Rasmus Holm <rasmus.holm@modularfinance.se>
2019-04-03 17:30:27 +0200

    frontend example

---
+++ ui/frontend0/app.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+    <div class="container">
+        Hello {{bundler}}
+        <button v-on:click="counter = counter + 1">Click</button>
+        {{counter}}
+    </div>
+</template>
+
+
+<script lang="ts">
+
+    import Vue from "vue";
+
+    export default Vue.extend({
+        data() {
+            return {
+                bundler: "Parcel and Vue",
+                counter: 0
+            };
+        },
+        methods: {
+            get: function (event) {
+
+            }
+        }
+    });
+</script>
+
+<style lang="scss" scoped>
+    .container {
+        color: green;
+    }
+</style>
